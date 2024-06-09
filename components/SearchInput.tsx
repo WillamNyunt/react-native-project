76eb1a4cbbd595a2ed7bfd56ad6006411d011951
William Nyunt <82210457+WillamNyunt@users.noreply.github.com>
2024-06-09 12:56:52 +0100

    Search query functionality

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { TextInput, TouchableOpacity, Image } from 'react-native'
 import { icons } from '@/constants'
+import { router, usePathname } from 'expo-router'
 
 type FormFieldProps = {
     title: string,
@@ -15,12 +16,19 @@ type FormFieldProps = {
 
 
 const SearchInput = ({ title, value, handleChangeText, otherStyles, keyboardType, secureTextEntry, placeholder, ...props }: FormFieldProps) => {
-    const [showPassword, setShowPassword] = useState(false)
+    const pathanme = usePathname()
+    const [query, setQuery] = useState('')
 
     return (
         <View className='flex-row border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center space-x-4'>
-            <TextInput className='åtext-base mt-0.5 text-white flex-1 font-pregular' value={value} placeholder={placeholder} placeholderTextColor={'#7b7b8b'} onChangeText={handleChangeText} secureTextEntry={title === 'Password' && !showPassword} />
-            <TouchableOpacity>
+            <TextInput className='text-base mt-0.5 text-white flex-1 font-pregular' value={value} placeholder={placeholder} placeholderTextColor={'#CDCDE0'} onChangeText={(e) => setQuery(e)} secureTextEntry={title === 'Password' && !showPassword} />
+            <TouchableOpacity onPress={() => {
+                if (!query) {
+                    return Alert.alert('Missing query', 'Please input something to search results across database')
+                }
+                if (pathanme.startsWith('/search')) router.push(`/search/${query}`)
+                else router.push(`/search/${query}`)    
+            }}>
                 <Image source={icons.search} className='w-5 h-5' resizeMode='contain' />
             </TouchableOpacity>
         </View>
