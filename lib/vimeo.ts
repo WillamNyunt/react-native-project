53501c7a5aa02c9dba2df291539ed53a37040eb8
William Nyunt <82210457+WillamNyunt@users.noreply.github.com>
2024-06-11 13:00:37 +0100

    More robust Vimeo fn

--- lib/vimeo.ts
+++ lib/vimeo.ts
@@ -1,30 +1,38 @@
 // Function to extract the video ID from the Vimeo URL
-const extractVimeoId = (url : string) => {
-    const regex = /player\.vimeo\.com\/video\/(\d+)/;
-    const match = url.match(regex);
-    return match ? match[1] : null;
-  };
-  
-  // Function to fetch video URL from Vimeo configuration
-  export const fetchVimeoUrl = async (url : string) => {
+const extractVimeoId = (url: string) => {
+  const regex = /player\.vimeo\.com\/video\/(\d+)/;
+  const match = url.match(regex);
+  return match ? match[1] : null;
+};
+
+// Function to fetch video URL from Vimeo configuration
+export const fetchVimeoUrl = async (url: string) => {
+
+  if (url.includes('vimeo')) {
+    console.log('Fetching Vimeo video URL:', url);
     const videoId = extractVimeoId(url);
     if (!videoId) {
       throw new Error('Invalid Vimeo URL');
     }
-  
+
     const configUrl = `https://player.vimeo.com/video/${videoId}/config`;
+
     try {
       const response = await fetch(configUrl);
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
       const data = await response.json();
-      const videoUrl = data.request.files.hls.cdns[Object.keys(data.request.files.hls.cdns)[0]].url;  
+      const videoUrl = data.request.files.hls.cdns[Object.keys(data.request.files.hls.cdns)[0]].url;
       return videoUrl;
     } catch (error) {
       console.error('Error fetching Vimeo video URL:', error);
       throw error;
     }
-  };
-  
-  
+  } else {
+    console.log(url)
+    return url;
+  }
+};
+
+
