258ca25d2ddce6c6742625f510acc7cce5d65753
William Nyunt <82210457+WillamNyunt@users.noreply.github.com>
2024-06-11 13:40:44 +0100

    Placeholder search input

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -5,13 +5,13 @@ import { icons } from '@/constants'
 import { router, usePathname } from 'expo-router'
 
 
-const SearchInput = ({initialQuery} : {initialQuery: string}) => {
+const SearchInput = ({initialQuery, placeholder} : {initialQuery: string, placeholder : string}) => {
     const pathanme = usePathname()
     const [query, setQuery] = useState(initialQuery || "");
 
     return (
         <View className='flex-row border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center space-x-4'>
-            <TextInput className='text-base mt-0.5 text-white flex-1 font-pregular' value={query} placeholder='Search a video or topic' placeholderTextColor={'#CDCDE0'} onChangeText={(e) => setQuery(e)} />
+            <TextInput className='text-base mt-0.5 text-white flex-1 font-pregular' value={query} placeholder={placeholder ? placeholder : 'Search'} placeholderTextColor={'#CDCDE0'} onChangeText={(e) => setQuery(e)} />
             <TouchableOpacity onPress={() => {
                 if (query === "") {
                     return Alert.alert('Missing query', 'Please input something to search results across database')
